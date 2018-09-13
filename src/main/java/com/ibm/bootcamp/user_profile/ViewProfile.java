fa4d21f9bb5c11b13f67ec65bc215b9924af523f
Lance Alunan <lance.alunan@gmail.com>
2018-09-13 16:08:52 +0800

    updated

--- src/main/java/com/ibm/bootcamp/user_profile/ViewProfile.java
+++ src/main/java/com/ibm/bootcamp/user_profile/ViewProfile.java
@@ -18,7 +18,6 @@ public class ViewProfile {
 	@RequestMapping("/viewProfile")
 	public List<User> view(@RequestBody Map<String, Object> request) {
 		
-	   	
 		return UserDAO.viewProfile(request);
 	}
 }
